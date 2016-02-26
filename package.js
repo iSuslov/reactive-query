87849fb6856eaab328b20911874ef34c74f888c4
Ivan Suslov <digitalzuzel@gmail.com>
2016-02-27 03:46:15 +0600

    Version change

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zuzel:reactive-query',
-  version: '1.1.1',
+  version: '1.1.2',
   summary: 'Package that makes it easy to serialize application state in URL query params',
   git: 'https://github.com/iSuslov/reactive-query',
   documentation: 'README.md'
