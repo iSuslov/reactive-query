1e117a2525e3554380a8434fe90f127f7bb5684f
Ivan Suslov <digitalzuzel@gmail.com>
2016-02-27 04:06:43 +0600

    Minor

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'zuzel:reactive-query',
-  version: '1.1.4',
+  version: '1.1.5',
   summary: 'Package that makes it easy to serialize application state in URL query params',
   git: 'https://github.com/iSuslov/reactive-query',
   documentation: 'README.md'
